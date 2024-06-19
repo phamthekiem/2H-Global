5172bbd6758a406e489cc07d7d20c0bdd748d5cc
kiempt <phamthekiem193@gmail.com>
2024-06-19 16:08:52 +0700

    base theme

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<?php global $sh_option;?>
+<body <?php body_class(); ?> itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<?php do_action( 'sh_before_header' );?>
+
+<div id="page" class="site">
+
+	<header id="masthead" <?php header_class();?> role="banner" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
+
+		<!-- Start Top Header -->
+		<?php if( $sh_option['display-topheader-widget'] == 1 ) : ?>
+			<div class="top-header">
+				<div class="container">
+					<?php dynamic_sidebar( 'Top Header' );?>
+				</div>
+			</div>
+		<?php endif; ?>
+		<!-- End Top Header -->
+
+		<?php sh_header_layout();?>
+
+	</header><!-- #masthead -->
+	
+	<div id="content" class="site-content">
+
+	<?php do_action( 'before_loop_main_content' ) ?>
+
+		<div class="container">
---
+++ pages-template/front-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Trang chủ
+ *
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package SH_Theme
+ */
+
+global $sh_option;
+get_header(); ?>
+
+	
+
+<?php
+get_footer();
